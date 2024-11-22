1c977a335d43d30d1e1dc736097230c571eb6178
Md. Arman Hossen <md.armanhossen@Mds-MacBook-Air.local>
2024-11-22 23:55:29 +0600

    test

--- app.py
+++ app.py
@@ -79,5 +79,3 @@ def logout():
 if __name__ == '__main__':
     app.secret_key = 'your-secret-key'  # Replace with a secure key
     app.run(debug=True)
-
-//jdfksgklgjfjtieoriweriuweuiosdjfowiqeofkdj
