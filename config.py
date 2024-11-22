38c7335ecf417841941e9b518235b49d243dd002
Md. Arman Hossen <md.armanhossen@Mds-MacBook-Air.local>
2024-11-23 02:23:13 +0600

    clear main branch

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'your-secret-key'  # Replace with a secure key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # Database
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
